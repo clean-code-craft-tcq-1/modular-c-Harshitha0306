a10abab77433853147d70207535041b50328fde6
Harshitha0306 <77484111+Harshitha0306@users.noreply.github.com>
2021-02-25 19:39:42 +0530

    Update telecolor_convert.h

--- telecolor_convert.h
+++ telecolor_convert.h
@@ -34,6 +34,6 @@ const int MAX_COLORPAIR_NAME_CHARS = 16;
 
 ColorPair GetColorFromPairNumber(int pairNumber);
 int GetPairNumberFromColor(const ColorPair* colorPair);
-void ColorPairToString(const ColorPair* colorPair, char* buffer);
+void ConvertColorPairToString(const ColorPair* colorPair, char* buffer);
 
 #endif  /* telecolor_convert_h */
