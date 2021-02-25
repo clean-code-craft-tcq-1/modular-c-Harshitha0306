f3d6cca3e99c9f7dfe4529d58f4a062fb7670a58
Harshitha0306 <77484111+Harshitha0306@users.noreply.github.com>
2021-02-25 19:32:09 +0530

    Add files via upload

---
+++ manual_reference.c
@@ -0,0 +1,36 @@
+/*
+ *
+ ***************************************************************************************************
+ * Author:             Harshitha Subramani
+ * Date:               23/02/2020  
+ * Filename:           main.c
+ *
+ *****************************************************************************************************
+*
+***************************************************************************************
+* Includes
+***************************************************************************************
+*/
+
+#include <stdio.h>
+#include "telecolor_test.h"
+
+/*
+*****************************************************************************************
+ * Function:int main()
+ * Description: Main function of main.c file.Program execution starts from here.  
+ * The function calls the Test functions which prints color coding pair that can be used
+ * as a reference manual for wiring personnel.
+ *
+ *****************************************************************************************
+ */
+ 
+int main() {
+    testNumberToPair(4, WHITE, BROWN);
+    testNumberToPair(5, WHITE, SLATE);
+
+    testPairToNumber(BLACK, ORANGE, 12);
+    testPairToNumber(VIOLET, SLATE, 25);
+	DisplayManualReference();
+    return 0;
+}
